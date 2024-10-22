cdf6eceb7ddf9fb3974b77b4d72c992fb125be42
Samar Hayat <samarhayat.dev@gmail.com>
2024-10-22 09:02:35 +0500

    update

--- src/components/SidebarHeader.tsx
+++ src/components/SidebarHeader.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
-
+import me from "public/images/me.png";
+import gogencyImage2 from "public/images/sidefolio-aceternity-2.png";
 export const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        src={me}
         alt="Avatar"
         height="40"
         width="40"
