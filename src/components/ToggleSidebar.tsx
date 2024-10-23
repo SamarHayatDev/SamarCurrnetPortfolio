da5881ba0ff54dca74d461f42afde5ecf12bc35d
Samar Hayat <samarhayat.dev@gmail.com>
2024-10-24 00:09:37 +0500

    update readme.md and metadata

--- src/components/ToggleSidebar.tsx
+++ src/components/ToggleSidebar.tsx
@@ -34,11 +34,23 @@ export const ToggleSidebar = () => {
         )}
       </AnimatePresence>
 
-      <button
+      {/* <button
         className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center z-50"
         onClick={() => setOpen(!open)}
       >
         <IconLayoutSidebarRightCollapse className="h-4 w-4 text-secondary" />
+      </button> */}
+      <button
+        className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-white/10 bg-slate-900 shadow-2xl shadow-zinc-900 rounded-full backdrop-blur-sm flex items-center justify-center z-50 group"
+        onClick={() => setOpen(!open)}
+      >
+        <span className="absolute inset-0 overflow-hidden rounded-full">
+          <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+        </span>
+        <div className="relative flex items-center justify-center z-10">
+          <IconLayoutSidebarRightCollapse className="h-4 w-4 text-white" />
+        </div>
+        <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
       </button>
     </>
   );
