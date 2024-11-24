1537cdb9de1052355ff38a83250f777a6aafbbd2
Samar Hayat <samarhayat.dev@gmail.com>
2024-11-24 19:27:10 +0500

    other tech stack add where im not pro but can work with error fix

--- src/components/NotAProBut.tsx
+++ src/components/NotAProBut.tsx
@@ -36,7 +36,7 @@ export const NotAProBut = () => {
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-10 mb-4"
       >
-        I'm Not a Pro, But I Can Work With...
+        I&apos;m Not a Pro, But I Can Work With...
       </Heading>
       <div className="flex flex-wrap">
         {tools.map((item) => (
