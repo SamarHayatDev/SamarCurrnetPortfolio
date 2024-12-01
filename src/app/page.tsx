a9f385579b92c8ddca92d3d3ea9ee8e517164223
Samar Hayat <samarhayat.dev@gmail.com>
2024-12-01 13:36:26 +0500

    add icons for hire me :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { NotAProBut } from "@/components/NotAProBut";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { TechStack } from "@/components/TechStack";
+import Link from "next/link";
+import { IconMail } from "@tabler/icons-react";
 
 export default function Home() {
   return (
@@ -29,6 +31,18 @@ export default function Home() {
         contributed to <Highlight>open-source</Highlight> projects benefiting
         developers worldwide.
       </Paragraph>
+      {/* group-hover/button:bg-gray-50/15 group-hover/button:scale-105 hover:scale-105*/}
+      <Link
+        href={"mailto:samarhayat.dev@gmail.com"}
+        // target="__blank"
+        className="inline-flex mt-4 items-center gap-1 group/button rounded-full  focus:outline-none transition ring-offset-gray-900 bg-gray-800 text-white shadow-lg shadow-black/20 sm:backdrop-blur-sm  focus-visible:ring-1 focus-visible:ring-offset-2 ring-gray-50/60 text-sm font-medium px-4 py-2 origin-left
+        group-hover/button:bg-gray-50/15 group-hover/button:scale-105 hover:scale-105
+        
+        "
+      >
+        <IconMail />
+        Hire Me
+      </Link>
       <Heading
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
